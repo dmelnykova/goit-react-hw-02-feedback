fe291334f5659b1b0130e7c302dd4c3742ba17a9
dariameln1 <dariamelnykova1@gmail.com>
2023-11-18 14:41:09 +0100

    preparation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+
+import { Component } from "react";
+// import { Section } from './components/Section';
+// import { Notification } from 
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  onUpdated = item => {
+    this.setState(prevState => {
+      return {
+        [item]: prevState[item] + 1
+      };
+    })
+  } 
+}
